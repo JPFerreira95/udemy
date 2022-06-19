77dd8b49944a84c17052f2187b3001ab98d58fdf
João Ferreira <joaopsf95@gmail.com>
2022-06-19 21:34:37 +0100

    added java doc

--- master-hibernate-and-jpa/src/main/java/com/udemy/master/hibernate/jpa/controller/book/BookController.java
+++ master-hibernate-and-jpa/src/main/java/com/udemy/master/hibernate/jpa/controller/book/BookController.java
@@ -8,9 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.udemy.master.hibernate.jpa.entity.book.Book;
 
+/**
+ * The Class BookController.
+ */
 @RestController
 public class BookController {
 
+	/**
+	 * Gets the all books.
+	 *
+	 * @return the all books
+	 */
 	@GetMapping("/books")
 	public List<Book> getAllBooks() {
 		return Arrays.asList(new Book(1L, "Harry Potter: and the philosopher's stone", "J.K. Rolling"));
