54fc58ceee227a4097ad6950954ca988aeadb176
Joao Ferreira <joaopsf95@gmail.com>
2023-09-17 20:23:36 +0100

    Added circuit breaker to currency-exchange

--- currency-exchange-service/src/main/java/com/udemy/microservices/currencyexchangeservice/controller/circuitbreaker/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/udemy/microservices/currencyexchangeservice/controller/circuitbreaker/CircuitBreakerController.java
@@ -1,13 +1,30 @@
 package com.udemy.microservices.currencyexchangeservice.controller.circuitbreaker;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
 
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
     @GetMapping("/sample-api")
+    // @Retry(name = "default", fallbackMethod = "hardcodedResponse") // This annotation does not work, don't know why and there's no responses on the internet
+    // @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse") // This annotation does not work, don't know why and there's no responses on the internet
     public String sampleApi() {
-        return "Sample API";
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+
+        return forEntity.getBody();
+    }
+
+    /*
+     * This method works like an exception handler for the retry annotation
+     */
+    private String hardcodedResponse(Exception ex) {
+        return "fallback-response";
     }
 }
