66c685818b60a79a7bd9d3172de8671c9431deb0
Joao Ferreira <joaopsf95@gmail.com>
2023-12-26 23:04:22 +0000

    Move microservices projects into master_microservices

--- currency-exchange-service/src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO currency_exchange
-    (id, currency_from, currency_to, conversion_multiple)
-VALUES
-    (1001, 'USD', 'INR', 65),
-    (1002, 'EUR', 'INR', 75),
-    (1003, 'AUD', 'INR', 25);
--- master-microservices/src/main/java/com/udemy/microservices/post/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.udemy.microservices.post;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Integer> {
-
-}
